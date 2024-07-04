0d7cb02ad7765dcfc2c9055e5ebd48974d68def9
ferdinand@creanimo.net <ferdinand@creanimo.net>
2024-07-04 10:16:21 +0000

    Edited view_imageAssets

--- client_code/view_imageAssets/__init__.py
+++ client_code/view_imageAssets/__init__.py
@@ -10,9 +10,7 @@ class view_imageAssets(view_imageAssetsTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-    self.repeating_panel_1.items = (
-     {'from': 'Joe', 'subject': 'Latest tech report'},
-     {'from': 'Sally', 'subject': 'Movie night on Friday?'},
-     {'from': 'Ada', 'subject': 'My top 10 cat videos'},
-   )
+    self.repeating_panel_1.items = []
+
+    for row in app_tables.images.search():
+      self.repeating_panel_1.items.append({'name': row['name']})
